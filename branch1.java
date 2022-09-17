d6fd1664da9b5855b4c0080df63a74b5187a2b9b
personalpra <jawale.prasad@gmail.com>
2022-09-17 17:23:23 +0530

    first branch second commit

--- branch1.java
+++ branch1.java
@@ -1,6 +1,6 @@
 public class branch1 {
 
     public static void main(String[] args) {
-        System.out.println("branch 1 second commit" );
+        System.out.println("branch 1 first commit" );
     }
 }
